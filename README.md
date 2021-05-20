95ef7209b291b40c024fc9622cee5ae027e51fbe
keepthinker <keepthinker@gmail.com>
2021-05-20 22:31:59 +0800

    go array and slice

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-#  Learn some script languages
+#  Learn some languages
 1. Python
 2. Linux Shell
 3. Windows Shell
+4. Golang
---
+++ go-example/slice-array.go
@@ -0,0 +1,53 @@
+package main
+
+import "fmt"
+
+func main() {
+	var arr = []int{1, 2, 3, 4, 5, 6}
+	var lenArr = len(arr)
+	for i := 0; i < lenArr; i++ {
+		fmt.Printf("%d ", arr[i])
+	}
+	fmt.Println()
+	
+	var slice []int
+	printSlice(slice)
+
+	if (slice == nil) {
+		fmt.Println("slice is nil")
+	}
+	
+	var nums = make([]int, 10, 20)
+	printSlice(nums)
+
+	numbers := []int{1, 2, 3, 4, 5, 6, 7}
+	printSlice(numbers)
+
+	printSlice(numbers[1:6])
+
+	printSlice(numbers[:5])
+
+	printSlice(numbers[2:])
+
+
+	numbers1 := append(numbers, 8)
+	numbers[1] = 9
+	
+	printSlice(numbers)
+	printSlice(numbers1)
+
+	numbers1 = append(numbers1, 9 ,10, 11)
+	printSlice(numbers1)
+
+	finalNumbers := make([]int, len(numbers1), 100)
+	copy(finalNumbers, numbers1)
+
+	printSlice(finalNumbers)
+	
+	
+}
+
+func printSlice(s []int) {
+   fmt.Printf("len = %d cap = %d slice = %v\n", len(s), cap(s), s)
+}
+
